f8946d897a6de2d0974739bc8e4a9b9d70322b99
Rob Young <rob.young@digital.cabinet-office.gov.uk>
2012-08-14 11:25:04 +0100

    Update end_at parsing to handle date time
    Message start_at / end_at are now date boundary datetimes.
    
    author: @jthoenes
    author: @robyoung

--- lib/recorders/weekly_visits_recorder.rb
+++ lib/recorders/weekly_visits_recorder.rb
@@ -11,29 +11,19 @@ module Recorders
 
     def initialize(logger)
       @logger = logger
-      client = Bunny.new ENV['AMQP']
-      client.start
-      @queue = client.queue(ENV['QUEUE'] || 'weekly_visits')
-      exchange = client.exchange('datainsight', :type => :topic)
-
-      @queue.bind(exchange, :key => 'google_analytics.visits.weekly')
-      @logger.info("Bound to google_analytics.visits.weekly, listening for events")
-
-      @queue.bind(exchange, :key => 'google_drive.visits.weekly')
-      @logger.info("Bound to google_drive.visits.weekly, listening for events")
     end
 
     def run
-      @queue.subscribe do |msg|
+      queue.subscribe do |msg|
         @logger.debug("Received a message: #{msg}")
-        WeeklyVisitsRecorder.process_message(JSON.parse(msg[:payload], :symbolize_names => true))
+        process_message(JSON.parse(msg[:payload], :symbolize_names => true))
       end
     end
 
-    def self.process_message(msg)
+    def process_message(msg)
       weekly_visits = WeeklyVisits.first(
-          :start_at => Date.parse(msg[:payload][:start_at]),
-          :end_at => Date.parse(msg[:payload][:end_at]),
+          :start_at => parse_start_at(msg[:payload][:start_at]),
+          :end_at => parse_end_at(msg[:payload][:end_at]),
           :site => msg[:payload][:site]
       )
       if weekly_visits
@@ -42,11 +32,47 @@ module Recorders
       else
         WeeklyVisits.create(
             :value => msg[:payload][:value],
-            :start_at => Date.parse(msg[:payload][:start_at]),
-            :end_at => Date.parse(msg[:payload][:end_at]),
+            :start_at => parse_start_at(msg[:payload][:start_at]),
+            :end_at => parse_end_at(msg[:payload][:end_at]),
             :collected_at => DateTime.parse(msg[:envelope][:collected_at]),
             :site => msg[:payload][:site]
         )
+      end
+    end
+
+    private
+    def queue
+      @queue ||= create_queue
+    end
+
+    def create_queue
+      client = Bunny.new ENV['AMQP']
+      client.start
+      queue = client.queue(ENV['QUEUE'] || 'weekly_visits')
+      exchange = client.exchange('datainsight', :type => :topic)
+
+      queue.bind(exchange, :key => 'google_analytics.visits.weekly')
+      @logger.info("Bound to google_analytics.visits.weekly, listening for events")
+
+      queue.bind(exchange, :key => 'google_drive.visits.weekly')
+      @logger.info("Bound to google_drive.visits.weekly, listening for events")
+
+      queue
+    end
+
+    def parse_start_at(start_at)
+      Date.parse(start_at)
+    end
+
+    # This recorder stores start and end as dates while the message format uses date times on date boundaries (midnight).
+    # This means that the date may have to be shifted back
+    def parse_end_at(end_at)
+      end_at = DateTime.parse(end_at)
+      if (end_at.hour + end_at.minute + end_at.second) == 0
+        # up to midnight, so including the previous day
+        end_at.to_date - 1
+      else
+        end_at.to_date
       end
     end
 
--- test/recorders/weekly_visits_recorder_spec.rb
+++ test/recorders/weekly_visits_recorder_spec.rb
@@ -11,11 +11,12 @@ describe "WeeklyVisitsRecorder" do
         },
         :payload => {
             :value => 700,
-            :start_at => "28/3/2011",
-            :end_at => "3/4/2011",
+            :start_at => "2011-03-28T00:00:00",
+            :end_at => "2011-04-04T00:00:00",
             :site => "directgov"
         }
     }
+    @recorder = Recorders::WeeklyVisitsRecorder.new(nil)
   end
 
   after :each do
@@ -23,22 +24,39 @@ describe "WeeklyVisitsRecorder" do
   end
 
   it "should store weekly visits when processing drive message" do
-    Recorders::WeeklyVisitsRecorder.process_message(@message)
+    @recorder.process_message(@message)
 
     WeeklyVisits.all.should_not be_empty
+    item = WeeklyVisits.first
+    item.value.should == 700
+    item.start_at.should == Date.new(2011, 3, 28)
+    item.end_at.should == Date.new(2011, 4, 3)
+    item.site.should == "directgov"
   end
 
   it "should store weekly data when processing analytics message" do
     @message[:payload][:site] = "govuk"
-    Recorders::WeeklyVisitsRecorder.process_message(@message)
+    @recorder.process_message(@message)
 
     WeeklyVisits.all.should_not be_empty
+    item = WeeklyVisits.first
+    item.value.should == 700
+    item.start_at.should == Date.new(2011, 3, 28)
+    item.end_at.should == Date.new(2011, 4, 3)
+    item.site.should == "govuk"
+  end
+
+  it "should correctly handle end date over month boundaries" do
+    @message[:payload][:end_at] = "2011-09-01T00:00:00"
+    @recorder.process_message(@message)
+    item = WeeklyVisits.first
+    item.end_at.should == Date.new(2011, 8, 31)
   end
 
   it "should update existing measurements" do
-    Recorders::WeeklyVisitsRecorder.process_message(@message)
+    @recorder.process_message(@message)
     @message[:payload][:value] = 900
-    Recorders::WeeklyVisitsRecorder.process_message(@message)
+    @recorder.process_message(@message)
     WeeklyVisits.all.length.should == 1
     WeeklyVisits.first.value.should == 900
   end
