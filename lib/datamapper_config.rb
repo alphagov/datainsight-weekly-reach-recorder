6163142254fb6ec415f5fe46604334de9ccefc2b
Rob Young <rob.young@digital.cabinet-office.gov.uk>
2012-08-14 11:23:12 +0100

    Move data mapper configuration switch into config
    author: @jthoenes
    author: @robyoung

--- lib/datamapper_config.rb
+++ lib/datamapper_config.rb
@@ -2,6 +2,17 @@ require 'dm-constraints'
 require 'dm-migrations'
 
 module DataMapperConfig
+  def self.configure(env=ENV["RACK_ENV"])
+    case (env or "default").to_sym
+      when :test
+        DataMapperConfig.configure_test
+      when :production
+        DataMapperConfig.configure_production
+      else
+        DataMapperConfig.configure_development
+    end
+  end
+
   def self.configure_development
     DataMapper::Logger.new($stdout, :debug)
     DataMapper.setup(:default, 'mysql://root:@localhost/datainsights_weekly_visits')
