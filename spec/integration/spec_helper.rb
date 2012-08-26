7786580f548d79958e3815f1f91b597f0c6a854d
Pawel Badenski <Pawel.Badenski@thoughtworks.com>
2012-08-26 08:23:58 +0100

    Fix randomly failing test
    Author: @pbadenski

--- spec/integration/spec_helper.rb
+++ spec/integration/spec_helper.rb
@@ -5,19 +5,13 @@ require "rack/test"
 
 require_relative "../../lib/app"
 
-def create_sunday(date_time)
-  date_time - date_time.wday
-end
-
-def create_measurements(params={})
-  params[:start_at] ||= create_sunday((DateTime.now << 6) - 5)
-  params[:end_at] ||= (DateTime.now - 5)
-
-  end_at = params[:end_at]
-  while params[:start_at] < end_at
-    params[:end_at] = params[:start_at] + 7
+def create_measurements(start_at, end_at, params={})
+  while start_at < end_at
+    each_end_at = start_at + 7
+    params[:start_at] = start_at
+    params[:end_at] = each_end_at
     FactoryGirl.create(:model, params)
 
-    params[:start_at] += 7
+    start_at += 7
   end
 end
--- spec/integration/weekly_reach_spec.rb
+++ spec/integration/weekly_reach_spec.rb
@@ -11,29 +11,37 @@ describe "weekly-visitors" do
     WeeklyReach::Model.destroy!
   end
 
+  def last_sunday_of(date_time)
+    date_time - (date_time.wday == 0 ? 7 : date_time.wday)
+  end
+
   it "should return JSON data for weekly visitors" do
-    create_measurements(metric: "visitors", value: 500, site: "govuk")
-    create_measurements(metric: "visitors", value: 600, site: "directgov")
-    create_measurements(metric: "visitors", value: 700, site: "businesslink")
+    start_at ||= last_sunday_of(DateTime.now << 6)
+    end_at ||= last_sunday_of(DateTime.now)
+    end_date_of_a_first_week = (DateTime.now << 6) + 7
+
+    create_measurements(start_at, end_at, metric: "visitors", value: 500, site: "govuk")
+    create_measurements(start_at, end_at, metric: "visitors", value: 600, site: "directgov")
+    create_measurements(start_at, end_at, metric: "visitors", value: 700, site: "businesslink")
 
     get "/weekly-visitors"
     last_response.should be_ok
     response = JSON.parse(last_response.body, :symbolize_names => true)
 
     govuk = response[:govuk]
-    govuk.should have(26).items
-    govuk.first[:date].should == create_sunday((DateTime.now << 6)+7).to_date.strftime
+    govuk.should have(27).items
+    govuk.first[:date].should == last_sunday_of(end_date_of_a_first_week).to_date.strftime
     govuk.first[:value].should == 500
 
-    dgov = response[:directgov]
-    dgov.should have(26).items
-    dgov.first[:date].should == create_sunday((DateTime.now << 6)+7).to_date.strftime
-    dgov.first[:value].should == 600
+    directgov = response[:directgov]
+    directgov.should have(27).items
+    directgov.first[:date].should == last_sunday_of(end_date_of_a_first_week).to_date.strftime
+    directgov.first[:value].should == 600
 
-    dlink = response[:businesslink]
-    dlink.should have(26).items
-    dlink.first[:date].should == create_sunday((DateTime.now << 6)+7).to_date.strftime
-    dlink.first[:value].should == 700
+    businesslink = response[:businesslink]
+    businesslink.should have(27).items
+    businesslink.first[:date].should == last_sunday_of(end_date_of_a_first_week).to_date.strftime
+    businesslink.first[:value].should == 700
 
     response[:highlight_spikes].should == false
     response[:highlight_troughs].should == false
